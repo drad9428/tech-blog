852abe4a8f48054151ff3b3b1cc9042c4bead82c
drad <robertjdstoddard@gmail.com>
2021-09-27 16:45:23 -0600

    handlebars and public js

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,9 +1,21 @@
 async function deleteFormHandler(event){
     event.preventDefault()
 
-    await fetch(`/api/posts/${id}`, {
+    const id = window.location.toString().split('/')[
+        window.localStorage.toString().split('/').length - 1
+    ]
+    
+    const response = await fetch(`/api/posts/${id}`, {
         method: 'DELETE'
     })
+
+    if(response.ok) {
+        document.location.replace('/dashboard')
+    }
+    else {
+        alert(response.statusText)
+    }
+    
 }
 
 document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler)
